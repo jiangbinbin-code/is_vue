1e64e02e234565aa7ebd58c9fed05d30bdfc8a29
jiangbinbin-code <712041476@qq.com>
2021-10-26 21:17:33 +0800

    登录界面

--- fvue/src/router/index.js
+++ fvue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../views/Login'
+import Main from '../views/Main'
 // import HelloWorld from '@/components/HelloWorld'
 /*
 * 路由 做界面跳转
@@ -16,6 +17,14 @@ export default new Router({
       name: 'Login',
       // 路由跳转组件
       component: Login
+    },
+    {
+      // 路由跳转路径
+      path: '/main',
+      // 路由名称
+      name: 'Main',
+      // 路由跳转组件
+      component: Main
     }
   ]
 })
--- fvue/src/views/Login.vue
+++ fvue/src/views/Login.vue
@@ -1,15 +1,67 @@
 <template>
  <div>
-   我是登录页
+   <el-form ref ="form" :model="form" :rules="rules" class="login-box">
+     <h3 class="login-title"> 欢迎登录 </h3>
+     <el-form-item label="账号" prop="name">
+       <el-input type="text" aria-placeholder="请输入用户名" v-model="form.name"></el-input>
+     </el-form-item>
+     <el-form-item label="密码" prop="password">
+       <el-input type="password" aria-placeholder="请输入密码" v-model="form.password"></el-input>
+     </el-form-item>
+     <el-form-item >
+       <el-button type="primary"  @click="onSubmit">登录</el-button>
+     </el-form-item>
+   </el-form>
  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        name: '',
+        password: ''
+      },
+      rules: {
+        name: [{
+          required: true,
+          message: '请输入用户名',
+          trigger: 'blur'}],
+        password: [{
+          required: true,
+          message: '请输入密码',
+          trigger: 'blur'}]
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$route.push('/main')
+        } else {
+          console.log('请输入用户名或者密码')
+          return false
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .login-box{
+    width: 350px;
+    margin: 120px auto;
+    border: 1px solid #DCDFE6;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 30px #DCDFE6;
+  }
+  .login-title{
+    text-align: center;
+  }
 
 </style>
